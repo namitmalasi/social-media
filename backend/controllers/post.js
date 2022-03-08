d1a25d826b3394a8808abc877c67b00ad23ae75e
Namit Malasi <namitmalasi@gmail.com>
2022-03-08 12:37:41 +0530

    added the functionality to create a post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,4 +1,5 @@
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 exports.createPost = async (req, res) => {
   try {
@@ -8,12 +9,18 @@ exports.createPost = async (req, res) => {
         public_id: "req.body.public_id",
         url: "req.body.url",
       },
-      owner: "req.user_id",
+      owner: req.user._id,
     };
 
-    const newPost = await Post.create(newPostData);
+    const post = await Post.create(newPostData);
 
-    res.status(201).json({ success: true, post: newPost });
+    const user = await User.findById(req.user._id);
+
+    user.posts.push(post._id);
+
+    await user.save();
+
+    res.status(201).json({ success: true, post });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createPost } = require("../controllers/post");
+const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route("/post/upload").post(createPost);
+router.route("/post/upload").post(isAuthenticated, createPost);
 
 module.exports = router;
