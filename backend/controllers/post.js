52887311e6506ac529bb3b8b124be69978443bce
Namit Malasi <namitmalasi@gmail.com>
2022-03-11 19:53:12 +0530

    added deleting post functionality

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,6 +1,7 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
 
+// Creating a new post
 exports.createPost = async (req, res) => {
   try {
     const newPostData = {
@@ -26,6 +27,38 @@ exports.createPost = async (req, res) => {
   }
 };
 
+// deleting the post
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Post not found" });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+
+    await post.remove();
+
+    const user = await User.findById(req.user._id);
+
+    const index = user.posts.indexOf(req.params.id);
+
+    user.posts.splice(index, 1);
+    await user.save();
+
+    res.status(200).json({ success: true, message: "Post deleted" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// like and unlike the post
 exports.likeandUnlikePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { createPost, likeandUnlikePost } = require("../controllers/post");
+const {
+  createPost,
+  likeandUnlikePost,
+  deletePost,
+} = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
-router.route("/post/:id").get(isAuthenticated, likeandUnlikePost);
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeandUnlikePost)
+  .delete(isAuthenticated, deletePost);
 
 module.exports = router;
