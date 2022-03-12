37429645d6316b5cb25b88be91c5cf094547bdab
Namit Malasi <namitmalasi@gmail.com>
2022-03-12 15:09:11 +0530

    added follow and unfollow user functionality

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -50,7 +50,7 @@ exports.login = async (req, res) => {
     }
 
     const isMatch = await user.matchPassword(password);
-    
+
     if (!isMatch) {
       return res
         .status(400)
@@ -68,6 +68,44 @@ exports.login = async (req, res) => {
       .status(200)
       .cookie("token", token, options)
       .json({ success: true, user, token });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+// follow User
+
+exports.followUser = async (req, res) => {
+  try {
+    const userToFollow = await User.findById(req.params.id);
+    const loggedInUser = await User.findById(req.user._id);
+
+    if (!userToFollow) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+
+    if (loggedInUser.following.includes(userToFollow._id)) {
+      const indexFollowing = loggedInUser.following.indexOf(userToFollow._id);
+      const indexFollowers = userToFollow.followers.indexOf(loggedInUser._id);
+
+      loggedInUser.following.splice(indexFollowing, 1);
+      userToFollow.followers.splice(indexFollowers, 1);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      res.status(200).json({ success: true, message: "user Unfollowed" });
+    } else {
+      loggedInUser.following.push(userToFollow._id);
+      userToFollow.followers.push(loggedInUser._id);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      res.status(200).json({ success: true, message: "user followed" });
+    }
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { register, login } = require("../controllers/user");
+const { register, login, followUser } = require("../controllers/user");
+const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/follow/:id").get(isAuthenticated, followUser);
 
 module.exports = router;
