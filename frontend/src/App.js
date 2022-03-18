a388b1e52cc0d0c2b34b8c142718e921b5a14ef1
Namit Malasi <namitmalasi@gmail.com>
2022-03-18 20:33:43 +0530

    initialised front end and created header component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
+
+export default App;
