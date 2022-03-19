5d82f813b3f3882699ff9a09abe66cc72b977378
Namit Malasi <namitmalasi@gmail.com>
2022-03-19 21:00:49 +0530

    created the login page

---
+++ frontend/src/Actions/User.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const loginUser = (email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: "LoginRequest" });
+
+    const { data } = await axios.post(
+      "/api/v1/login",
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    dispatch({ type: "LoginSuccess", payload: data.user });
+  } catch (error) {
+    dispatch({ type: "LoginFailure", payload: error.response.data.message });
+  }
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <Router>
       <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
